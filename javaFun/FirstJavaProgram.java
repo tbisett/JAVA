23d41f464bd52ee9ce6ea6c501f5e9b3477f55c8
Taylor Bisett <taylorbisett@gmail.com>
2021-08-02 12:29:33 -0700

    iteration 1

---
+++ javaFun/FirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram {
+    public static void main (String[] args) {
+        System.out.println("My name is Taylor");
+        System.out.println("I am 26 years old");
+        System.out.println("I am from Seattle, Washington");
+    }
+}
